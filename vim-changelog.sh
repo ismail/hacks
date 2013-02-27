0579609be79d32403f08f0c87f4f4b0dcdbe8e9b
İsmail Dönmez <idoenmez@suse.de>
2013-02-27 12:11:16 +0100

    Fix matching multiline comments

--- vim-changelog.sh
+++ vim-changelog.sh
@@ -5,6 +5,6 @@ version=7.3
 
 echo "Updated to revision $2, fixes the following problems"
 for i in {$1..$2}; do
-    curl $directory/$version/$version.$i -s | grep Problem | \
-    sed s,"Problem:    ","  * ", | grep -v "Binary file (standard input) matches"
+    curl $directory/$version/$version.$i -s | grep -v "Binary file (standard input) matches" | \
+    tr '\n' ' '|grep -oP "Problem:.*Solution:"|sed s,"Problem:    ","  * ",|sed s,"Solution:",,|tr '\t' '\n'
 done
