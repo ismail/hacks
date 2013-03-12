d8c93123abc71968727bce05c87bf9838795e93f
İsmail Dönmez <idoenmez@suse.de>
2013-03-12 16:14:22 +0100

    apod.sh

---
+++ apod.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p /havana/apod
+
+image_path=`curl -s -L http://apod.nasa.gov/apod/|grep '<a href="image'|cut -f2 -d"\""`
+image_name=`echo $image_path|cut -f3 -d"/"`
+
+curl -s http://apod.nasa.gov/apod/$image_path -o /havana/apod/$image_name
+
+gsettings set org.gnome.desktop.background picture-uri file:///havana/apod/$image_name
+echo "Done."
+
