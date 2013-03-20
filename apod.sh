1d5a34cda1de90781996e0686e5807ac1dedc769
İsmail Dönmez <idoenmez@suse.de>
2013-03-20 16:57:12 +0100

    Use dropbox folder

--- apod.sh
+++ apod.sh
@@ -5,8 +5,8 @@ mkdir -p /havana/apod
 image_path=`curl -s -L http://apod.nasa.gov/apod/|grep '<a href="image'|cut -f2 -d"\""`
 image_name=`echo $image_path|cut -f3 -d"/"`
 
-curl -s http://apod.nasa.gov/apod/$image_path -o /havana/apod/$image_name
+curl -s http://apod.nasa.gov/apod/$image_path -o /havana/Dropbox/Pictures/apod/$image_name
 
-gsettings set org.gnome.desktop.background picture-uri file:///havana/apod/$image_name
+gsettings set org.gnome.desktop.background picture-uri file:///havana/Dropbox/Pictures/apod/$image_name
 echo "Done."
 
