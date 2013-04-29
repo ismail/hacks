cfe8d1cb180c2d29ef3655fc0d54d918732cdde8
İsmail Dönmez <ismail@donmez.ws>
2013-04-29 11:39:29 +0200

    small fixes to apod.sh

--- apod.sh
+++ apod.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir -p /havana/apod
-
+destination=/havana/btsync/Pictures/apod/
 image_path=`curl -s -L http://apod.nasa.gov/apod/|grep -m 1 '<a href="image'|cut -f2 -d"\""`
 image_name=`echo $image_path|cut -f3 -d"/"`
 
-curl -s http://apod.nasa.gov/apod/$image_path -o /havana/Dropbox/Pictures/apod/$image_name
+mkdir -p $destination
+curl -s http://apod.nasa.gov/apod/$image_path -o $destination/$image_name
 
-gsettings set org.gnome.desktop.background picture-uri file:///havana/Dropbox/Pictures/apod/$image_name
+gsettings set org.gnome.desktop.background picture-uri file://$destination/$image_name
 echo "Done."
 
