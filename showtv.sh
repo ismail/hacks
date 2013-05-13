e2911ac63c3b02bfa4732411c0859ad1d41680a5
İsmail Dönmez <ismail@donmez.ws>
2013-05-13 13:24:50 +0200

    Add the boring showtv.sh

---
+++ showtv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Interestingly, secret is not static
+secret=`curl -s -D- "http://www.showtvnet.com/player/index.asp?ptype=1&product=showtv"|grep -oP "(showtv3\?\S{68})"`
+
+rtmpdump -v -r "rtmp://fml.45FB.edgecastcdn.net/2045FB/showtv3" -a "2045FB/showtv3" -f "WIN 11,1,102,55" -W "http://www.showtvnet.com/skins/streamplayer.swf?v1.2" -p "http://www.showtvnet.com/player/index.asp?ptype=1&product=showtv" -y "$secret" -live - | vlc -
