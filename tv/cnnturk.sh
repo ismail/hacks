26cb96bc5739fdb8afb0421eb45c1323461083b7
İsmail Dönmez <ismail@donmez.ws>
2013-05-17 13:45:35 +0200

    Minor fix

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 avplay rtmp://live.dtv.cubecdn.net:80/cnnturktv app=cnnturktv swfUrl=http://tv.cnnturk.com/content/playermed/midyo/mplayer.v3.swf pageUrl=http://video.cnnturk.com/canli-yayin playpath=CNNTurk3 live=1 -window_title CNNTürk
