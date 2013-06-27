a3e3ae299f871143968994e417fbc6910e24d0c0
İsmail Dönmez <ismail@donmez.ws>
2013-06-27 13:14:28 +0200

    Url fix

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-vlc "http://live.netd.com.tr//S1/HLS_LIVE/cnn_turk/1500/prog_index.m3u8?key=0ac0b403a92d06220e54883c1c9e1087&live=true"
+vlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/1500/prog_index.m3u8?key=0ac0b403a92d06220e54883c1c9e1087&live=true"
