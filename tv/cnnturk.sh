112730384069d61978b64d8388ed9a0f0cb40e4f
İsmail Dönmez <ismail@donmez.ws>
2013-07-09 22:17:25 +0200

    Find key automatically

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/1500/prog_index.m3u8?key=0ac0b403a92d06220e54883c1c9e1087&live=true"
+key=`curl -s "http://video.cnnturk.com/actions/Video/NetDPlayer" | grep -oP '(?<=index.m3u8\?key=).*?(?=&live=)'`
+
+cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/1500/prog_index.m3u8?key=$key&live=true"
