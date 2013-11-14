9a172fa2a3bb04502c234334c9c9b28418ae54db
İsmail Dönmez <ismail@donmez.ws>
2013-11-14 11:34:24 +0200

    Update for new vim patches

--- vim-changelog.sh
+++ vim-changelog.sh
@@ -6,5 +6,6 @@ version=7.4
 echo "Updated to revision $2, fixes the following problems"
 for i in {$1..$2}; do
     curl $directory/$version/$version.$i -s | grep -v "Binary file (standard input) matches" | \
-    tr -d '\n' | grep -oP "Problem:.*Solution:" | sed s,"Problem:    ","  * ", | sed s,"Solution:",, | tr '\t' '\n'
+    tr -d '\n' | grep -oP "Problem:.*Solution:" | sed s,"Problem:    ","  * ", | sed s,"Solution:",, | \
+    tr '\t' '\n' | sed  s,'           ','',
 done
