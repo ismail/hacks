70a878b799a84b562f5d35460c36202f34e33adc
İsmail Dönmez <ismail@donmez.ws>
2013-12-16 23:00:06 +0200

    Update CNNTurk

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-key=`curl -s "http://video.cnnturk.com/actions/Video/NetDPlayer" | grep -oP '(?<=index.m3u8\?key=).*?(?=&live=)'`
+key=`curl -s "http://www.cnnturk.com/player/embed/51cc1dbd32dc9f19b8bc77cf"|grep key|cut -f2 -d"="|cut -f1 -d"&"`
 
-cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/1500/prog_index.m3u8?key=$key&live=true"
+cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/index.m3u8?key=$key&live=true"
