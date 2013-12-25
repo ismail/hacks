fc6aa2084ff3d9e8e3ab1bfc16494a0cca401e3f
İsmail Dönmez <ismail@donmez.ws>
2013-12-25 16:31:41 +0200

    Improve

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-path=`curl -s "http://www.cnnturk.com/player/embed/51cc1dbd32dc9f19b8bc77cf"|grep path|cut -f2 -d"'"|sed s,"amp;","",g`
+subpage=`curl -s http://www.cnnturk.com/canli-yayin|grep embed|cut -f4 -d"/"|cut -f1 -d '"'`
+key=`curl -s "http://www.cnnturk.com/player/embed/$subpage"|grep key|cut -f2 -d"="|cut -f1 -d"&"`
+bitrate=1500
 
-if [ -z $path ]; then
+if [ -z $key ]; then
     echo "Failed to get play path.";
 fi
 
-cvlc "http://live.netd.com.tr/$path"
+cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/$bitrate/prog_index.m3u8?key=$key&live=true"
