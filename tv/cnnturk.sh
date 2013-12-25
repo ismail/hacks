81a56157a99fb9c512eeb05821eebc3f8e0a84ff
İsmail Dönmez <ismail@donmez.ws>
2013-12-25 11:41:07 +0200

    Error handling

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -2,4 +2,8 @@
 
 path=`curl -s "http://www.cnnturk.com/player/embed/51cc1dbd32dc9f19b8bc77cf"|grep path|cut -f2 -d"'"|sed s,"amp;","",g`
 
+if [ -z $path ]; then
+    echo "Failed to get play path.";
+fi
+
 cvlc "http://live.netd.com.tr/$path"
