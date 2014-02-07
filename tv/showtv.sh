a1071aa659bd23c79c58954370da6beb3265db74
İsmail Dönmez <ismail@donmez.ws>
2014-02-07 14:42:30 +0200

    No longer need a secret

--- tv/showtv.sh
+++ tv/showtv.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# Interestingly, secret is not static
-secret=`curl -s -D- "http://www.showtvnet.com/player/index.asp?ptype=1&product=showtv"|grep -oP "(showtv2\?\S{68})"`
-
 rtmpdump -r "rtmp://mn-l.mncdn.com/showtv/" -a "showtv/" -f "LNX 12,0,0,44" -W "http://static.oroll.com/p.swf" -p "http://www.showtv.com.tr/canli-yayin/showtv" -y "showtv2" - | vlc -
