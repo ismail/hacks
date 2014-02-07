9622062bfe198c20de295ce1a7ed406b14977514
İsmail Dönmez <ismail@donmez.ws>
2014-02-07 14:36:20 +0200

    Use 1k as max bitrate

--- tv/cnnturk.sh
+++ tv/cnnturk.sh
@@ -2,10 +2,10 @@
 
 subpage=`curl -s http://www.cnnturk.com/canli-yayin|grep embed|cut -f4 -d"/"|cut -f1 -d '"'`
 key=`curl -s "http://www.cnnturk.com/player/embed/$subpage"|grep key|cut -f2 -d"="|cut -f1 -d"&"`
-bitrate=1500
+bitrate=1000
 
 if [ -z $key ]; then
     echo "Failed to get play path.";
 fi
 
-cvlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/$bitrate/prog_index.m3u8?key=$key&live=true"
+vlc "http://live.netd.com.tr/S1/HLS_LIVE/cnn_turk/$bitrate/prog_index.m3u8?key=$key&live=true"
