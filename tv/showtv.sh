6ac3bc36eb6228e1ac32a6a1a7942e06df0163ef
İsmail Dönmez <ismail@donmez.ws>
2014-02-07 14:41:29 +0200

    Fix showtv.sh

--- tv/showtv.sh
+++ tv/showtv.sh
@@ -3,4 +3,4 @@
 # Interestingly, secret is not static
 secret=`curl -s -D- "http://www.showtvnet.com/player/index.asp?ptype=1&product=showtv"|grep -oP "(showtv2\?\S{68})"`
 
-rtmpdump -r "rtmp://fml.45FB.edgecastcdn.net/2045FB/showtv2" -a "2045FB/showtv2" -f "WIN 11,1,102,55" -W "http://www.showtvnet.com/skins/streamplayer.swf?v1.2" -p "http://www.showtvnet.com/player/index.asp?ptype=1&product=showtv" -y "$secret" --live - | avplay -
+rtmpdump -r "rtmp://mn-l.mncdn.com/showtv/" -a "showtv/" -f "LNX 12,0,0,44" -W "http://static.oroll.com/p.swf" -p "http://www.showtv.com.tr/canli-yayin/showtv" -y "showtv2" - | vlc -
