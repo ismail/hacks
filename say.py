fe3ec51e2a7a315108cc442270014b4745207ba3
İsmail Dönmez <ismail@donmez.ws>
2014-05-28 13:15:14 +0300

    Use mpv

--- say.py
+++ say.py
@@ -4,12 +4,12 @@ import os
 import sys
 
 url="http://translate.google.com/translate_tts?tl="
-userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
-           Chrome/29.0.1541.0 Safari/537.36"
+userAgent="""Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
+             Chrome/29.0.1541.0 Safari/537.36"""
 
 lang=sys.argv[1]
 word="%22".join(sys.argv[2:])
 
 if __name__ == "__main__":
-    os.system('curl -s -A "%s" "%s%s&q="%s"" | mplayer2 - &> /dev/null' % (userAgent, url, lang, word))
+    os.system('curl -s -A "%s" "%s%s&q="%s"" | mpv - &> /dev/null' % (userAgent, url, lang, word))
 
