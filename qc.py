465a0e8724e2a2491fafc3c503208742e73c3564
İsmail Dönmez <ismail@i10z.com>
2014-08-05 16:09:17 +0300

    Be floatful

--- qc.py
+++ qc.py
@@ -5,7 +5,7 @@ import sys
 
 if len(sys.argv) > 1:
     input_string = " ".join(sys.argv[1:])
-    code = compile("from math import *\nresult = %s" % input_string, '<string>', 'exec')
+    code = compile("from __future__ import division\nfrom math import *\nresult = %s" % input_string, '<string>', 'exec')
     ns = {}
     exec code in ns
     print(ns["result"])
