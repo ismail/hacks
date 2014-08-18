20993f24d4136e40c3b5241250458ddca29e4986
İsmail Dönmez <ismail@i10z.com>
2014-08-18 12:44:58 +0300

    Update clang-arm build script

--- build-arm-clang.sh
+++ build-arm-clang.sh
@@ -5,17 +5,14 @@ version=3.6
 target=armv7l-unknown-linux-gnueabihf
 
 cd $root &&
-svn up && svn up tools/clang && svn up projects/compiler-rt && svn up projects/libcxx && svn up projects/libcxxabi &&
+svn up && svn up tools/clang && svn up tools/clang/tools/extra && svn up projects/compiler-rt && svn up projects/libcxx && svn up projects/libcxxabi &&
 
 rm -rf build && mkdir build && cd build &&
 
-CC=arm-clang CXX=arm-clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_TARGET_ARCH=ARM -DLLVM_TARGETS_TO_BUILD=ARM -DLLVM_DEFAULT_TARGET_TRIPLE=$target -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF -DENABLE_CLANG_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -G "Ninja" .. &&
+CC=arm-clang CXX=arm-clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_TARGET_ARCH=ARM -DLLVM_TARGETS_TO_BUILD=all -DLLVM_DEFAULT_TARGET_TRIPLE=$target -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DENABLE_CLANG_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -G "Ninja" .. &&
 
 ninja &&
 ninja install/strip &&
-echo -n "Fixing interpreter... " &&
-for f in `find llvm -type f`;do patchelf --set-interpreter /lib/ld-linux-armhf.so.3 $f &>/dev/null;done &&
-echo "Done." &&
 echo "Creating the tarball..." &&
 tar cJf llvm-armv7-$version-r`svnversion ../CREDITS.TXT`.tar.xz llvm &&
 echo -n "Uploading to Mega... " &&
