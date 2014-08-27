777f03b1ea7374a127170da381e0d1bfdab91820
İsmail Dönmez <ismail@i10z.com>
2014-08-27 09:33:06 +0300

    Whoops

--- build-arm-clang.sh
+++ build-arm-clang.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 root=/havana/llvm
-svnversion=`svnversion $root/CREDITS.TXT`
 version=3.6
 target=armv7l-unknown-linux-gnueabihf
 
 cd $root &&
 svn up && svn up tools/clang && svn up tools/clang/tools/extra && svn up projects/compiler-rt && svn up projects/libcxx && svn up projects/libcxxabi &&
 
+svnversion=`svnversion $root/CREDITS.TXT`
 rm -rf build && mkdir build && cd build &&
 
 CC=arm-clang CXX=arm-clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_TARGET_ARCH=ARM -DLLVM_TARGETS_TO_BUILD=all -DLLVM_DEFAULT_TARGET_TRIPLE=$target -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DENABLE_CLANG_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -G "Ninja" .. &&
