a13d25c84ec9d35244f1a462d0277f530e1bb44b
İsmail Dönmez <ismail@i10z.com>
2014-09-11 15:29:01 +0300

    Less boilerplate

--- noip.sh
+++ noip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-username=
+username=ismail@donmez.ws
 password=
-host=
+host=.ddns.net
 ip=`curl -s whatismyip.akamai.com`
 
 curl -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$host&myip=$ip"
