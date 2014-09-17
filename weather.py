f22c0e1d72773e3fa81a248da11be3346dee2cf3
İsmail Dönmez <ismail@i10z.com>
2014-09-17 15:02:36 +0300

    Update

--- weather.py
+++ weather.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 import urllib.request
 
 location="TR/Istanbul"
-apikey="ADDYOURAPIKEY"
+apikey=""
 url = "https://api.wunderground.com/api/%s/hourly/q/%s.json" % (apikey, location)
 hourlyForecast = {}
 
@@ -14,18 +15,23 @@ def getHourly():
 
     for entry in data["hourly_forecast"]:
         hour_string = "%s:%s" % (entry["FCTTIME"]["hour_padded"], entry["FCTTIME"]["min"])
-        temperature = entry["temp"]["metric"]
-        condition = entry["condition"]
 
         d = {}
-        d["temperature"] = temperature
-        d["condition"] = condition
+        d["temperature"] = entry["temp"]["metric"]
+        d["condition"] = entry["condition"]
+        d["rain"] = entry["pop"]
 
         hourlyForecast[hour_string] = d
 
 if __name__ == "__main__":
+    if not apikey:
+        print("Get an api key from https://wunderground.com/weather/api/d/pricing.html")
+        sys.exit(1)
+
     getHourly()
+
+    print("%7s%7s%10s %10s\n" % ("Time", u"\u2103", "Rain", "Conditions"))
     for key in sorted(hourlyForecast):
         d = hourlyForecast[key]
-        print("%8s%8s %10s" % (key, d["temperature"], d["condition"]))
+        print("%8s%8s%8s%% %10s" % (key, d["temperature"], d["rain"], d["condition"]))
 
