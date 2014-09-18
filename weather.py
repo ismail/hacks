9d6522d4d1018563f85e27f502e8008c84b27239
İsmail Dönmez <ismail@i10z.com>
2014-09-18 14:54:16 +0300

    Scrap yahoo weather data

--- weather.py
+++ weather.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python3
 
-import json
-import sys
-import urllib.request
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup as soup
 
-location="TR/Istanbul"
-apikey=""
-url = "https://api.wunderground.com/api/%s/hourly/q/%s.json" % (apikey, location)
-hourlyForecast = {}
-
-def getHourly():
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read().decode("utf-8"))
-
-    for entry in data["hourly_forecast"]:
-        hour_string = "%s:%s" % (entry["FCTTIME"]["hour_padded"], entry["FCTTIME"]["min"])
-
-        d = {}
-        d["temperature"] = entry["temp"]["metric"]
-        d["condition"] = entry["condition"]
-        d["rain"] = entry["pop"]
-
-        hourlyForecast[hour_string] = d
+url="http://in.weather.com/weather/hourByHour/Istanbul+34+Turkey+TUXX0014:1:TU?pagenum=2&nextbeginIndex=0"
 
 if __name__ == "__main__":
-    if not apikey:
-        print("Get an api key from https://wunderground.com/weather/api/d/pricing.html")
-        sys.exit(1)
+    result = urlopen(url).read()
+    resultTable=soup(result).findAll('div', attrs={"class" : "wx-timepart"})
 
-    getHourly()
+    print("\n%s%7s%10s %20s\n" % ("Time", u"\u2103", "Rain%", "Conditions"))
 
-    print("%7s%7s%10s %10s\n" % ("Time", u"\u2103", "Rain", "Conditions"))
-    for key in sorted(hourlyForecast):
-        d = hourlyForecast[key]
-        print("%8s%8s%8s%% %10s" % (key, d["temperature"], d["rain"], d["condition"]))
-
+    for result in resultTable:
+        hour = result.find('h3').text.split("\n")[0]
+        hour = "%s:%s" % (hour[:2], hour[2:])
+        condition = result.find('p', attrs={'class' : 'wx-phrase'}).text
+        temperature, humidity, rain, wind = [x.text.strip() for x  in result.findAll('dd')]
+        print("%s%9s%8s %20s" % (hour, temperature, rain, condition))
