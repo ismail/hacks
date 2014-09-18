db07d0e35fa3c1f10c5ca82cc934d5326fb4c387
İsmail Dönmez <ismail@i10z.com>
2014-09-18 16:14:51 +0300

    Set User-Agent

--- weather.py
+++ weather.py
@@ -8,10 +8,12 @@ from bs4 import BeautifulSoup as soup
 
 cityString = "Istanbul+34+Turkey+TUXX0014"
 url = ("http://in.weather.com/weather/hourByHour/%s?pagenum=2&nextbeginIndex=0" % cityString)
+ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2159.4 Safari/537.36'
 
 if __name__ == "__main__":
     request = Request(url)
     request.add_header('Accept-encoding', 'gzip,deflate')
+    request.add_header('User-Agent', ua)
     bytesIO = BytesIO(urlopen(request).read())
     data = GzipFile(fileobj=bytesIO, mode="rb").read()
     bs = soup(data)
