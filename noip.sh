c0a129bd9ad877400fc8df390a90abb62fcb5868
İsmail Dönmez <ismail@i10z.com>
2014-09-30 13:54:59 +0300

    Log errors

--- noip.sh
+++ noip.sh
@@ -5,4 +5,4 @@ password=
 host=.ddns.net
 ip=`curl -s whatismyip.akamai.com`
 
-curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$host&myip=$ip" &> /dev/null
+curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$host&myip=$ip" > /dev/null
