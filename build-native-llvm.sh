818d660d3b67b88cc3f437d3b84e86a3e9b61698
İsmail Dönmez <ismail@i10z.com>
2014-10-26 15:57:21 +0200

    A native llvm build script for ARM

---
+++ build-native-llvm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cd /havana/src/llvm
+svn up . tools/clang projects/compiler-rt projects/libcxx projects/libcxxabi
+cd build
+
+CFLAGS="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3-d16 -fuse-ld=gold" 
+CC=clang CXX=clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=/havana/dist/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCMAKE_C_FLAGS=$CFLAGS -DCMAKE_CXX_FLAGS=$CFLAGS -G "Ninja" .. 
+
+ninja
+
+# Test manually
+./bin/llvm-lit -s -j4 test
+./bin/llvm-lit -s -j4 tools/clang/test
+#./bin/llvm-lit -s -j4 projects/libcxx/test
+
+ninja install/strip
+
+version=3.6
+revision=`svnversion ../CREDITS.TXT`
+cd /havana/dist
+echo "Compressing with xz..."
+tar cJf llvm-armv7-$version-r"$revision".tar.xz llvm
+echo "Uploading..."
+scp llvm-armv7-$version-r"$revision".tar.xz i10z.com:/havana/llvm/checked
+ssh i10z.com ln -sf /havana/llvm/checked/llvm-armv7-$version-r"$revision".tar.xz  /havana/llvm/checked/latest
+rm llvm-armv7-*
+echo "llvm-armv7-$version-r"$revision".tar.xz uploaded."
