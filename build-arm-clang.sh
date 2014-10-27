34b4dad897f29b2eb5a922a5063a3d893bec05f8
İsmail Dönmez <ismail@i10z.com>
2014-10-27 16:15:48 +0200

    We can't check reliably with qemu

--- build-arm-clang.sh
+++ build-arm-clang.sh
@@ -15,12 +15,6 @@ rm -rf build; mkdir build; cd build
 CC=arm-clang CXX=arm-clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_TARGET_ARCH=ARM -DLLVM_TARGETS_TO_BUILD=all -DLLVM_DEFAULT_TARGET_TRIPLE=$target -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DLLVM_BUILD_EXAMPLES=OFF -DENABLE_CLANG_EXAMPLES=OFF -DLLVM_BUILD_TESTS=ON -G "Ninja" ..
 
 ninja
-
-# Check
-./bin/llvm-lit -s -j8 test
-./bin/llvm-lit -s -j8 tools/clang/test
-./bin/llvm-lit -s -j8 projects/libcxx/test
-
 ninja install/strip
 
 echo "Creating the tarball..."
