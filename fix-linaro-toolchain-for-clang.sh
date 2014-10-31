6725a6807ba9ae721b5d31d23589b5940c08f09d
İsmail Dönmez <ismail@i10z.com>
2014-10-31 18:05:42 +0200

    Fixes for the latest Linaro (2014.10) toolchain

--- fix-linaro-toolchain-for-clang.sh
+++ fix-linaro-toolchain-for-clang.sh
@@ -1,8 +1,4 @@
-LD_VERSION=2.19-2014.08
+LD_VERSION=2.20-2014.11-1-git
 
-cd $1
-ln -sf arm-linux-gnueabihf/libc/usr usr
-
-cd lib
-ln -sf ../arm-linux-gnueabihf/libc/lib/arm-linux-gnueabihf arm-linux-gnueabihf
-ln -sf ../arm-linux-gnueabihf/libc/lib/arm-linux-gnueabihf/ld-"$LD_VERSION".so ld-linux-armhf.so.3
+cd $1/lib
+ln -sf ld-"$LD_VERSION".so ld-linux-armhf.so.3
