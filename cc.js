5357965997250751fb15c1f22ccec03927c04938
İsmail Dönmez <ismail@i10z.com>
2014-11-22 16:08:28 +0200

    Format with clang-format

--- cc.js
+++ cc.js
@@ -2,27 +2,25 @@ var XMLHttpRequest = require('xhr2');
 var xhr = new XMLHttpRequest();
 
 function compile(data) {
-    xhr.open("POST", "http://coliru.stacked-crooked.com/compile", true);
-    xhr.onload = function (e) {
+  xhr.open("POST", "http://coliru.stacked-crooked.com/compile", true);
+  xhr.onload = function(e) {
     if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            console.log(xhr.responseText);
-        } else {
-            console.error(xhr.statusText);
-        }
-    }
-    };
-    
-    xhr.onerror = function (e) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
         console.error(xhr.statusText);
-    };
-    xhr.send(JSON.stringify({ "cmd": "clang++ -std=c++11 -stdlib=libc++ -O2 -Wall main.cpp && ./a.out", "src": data }));
+      }
+    }
+  };
+
+  xhr.onerror = function(e) { console.error(xhr.statusText); };
+  xhr.send(JSON.stringify({
+    "cmd": "clang++ -std=c++11 -stdlib=libc++ -O2 -Wall main.cpp && ./a.out",
+    "src": data
+  }));
 }
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.once('data', function (text) {
-    compile(text);
-});
-
+process.stdin.once('data', function(text) { compile(text); });
