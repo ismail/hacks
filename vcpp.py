fd9e673510c26d9d406130a579925a2a69a110f5
İsmail Dönmez <ismail@i10z.com>
2014-11-27 14:45:46 +0200

    Add a User-Agent

--- vcpp.py
+++ vcpp.py
@@ -30,7 +30,8 @@ def compileCode(code):
         "Accept": "text/html",
         "Content-type": "application/x-www-form-urlencoded",
         "Origin": "http://webcompiler.cloudapp.net",
-        "Referer": "http://webcompiler.cloudapp.net/"
+        "Referer": "http://webcompiler.cloudapp.net/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"
     }
 
     req = request.Request("http://webcompiler.cloudapp.net/",
