5e979b8c00739984b5f1afb1e0938428f78d1ecd
İsmail Dönmez <ismail@i10z.com>
2014-11-28 21:16:17 +0200

    Let c9 handle multiple URLs

--- c9.sh
+++ c9.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 # Simple downloader for channel9.msdn.com
 
-tmp=`mktemp`
-curl -s $1 -o $tmp
-url=`grep -oP '(?<=<a href=").*(?=\">High Quality WMV</a>)' $tmp`
-title=`grep -oP '(?<=<meta name="title" content=").*(?="/>)' $tmp`
-rm $tmp
-curl -C - -# $url -o "$title.wmv"
+set -euo pipefail
+IFS=$'\n\t'
+
+download_url() {
+    tmp=`mktemp`
+    curl -s $1 -o $tmp
+    url=`grep -oP '(?<=<a href=").*(?=\">High Quality WMV</a>)' $tmp`
+    title=`grep -oP '(?<=<meta name="title" content=").*(?="/>)' $tmp`
+    rm $tmp
+    curl -C - -# $url -o "$title.wmv"
+}
+
+for url in "$@"; do
+    download_url $url
+done
