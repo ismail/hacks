e38ef15a113820f01fd06e9c5a1d6e52d701e3f9
İsmail Dönmez <ismail@i10z.com>
2014-11-29 19:21:24 +0200

    Improve title parsing

--- c9.sh
+++ c9.sh
@@ -8,11 +8,14 @@ download_url() {
     tmp=`mktemp`
     curl -s $1 -o $tmp
     url=`grep -oP '(?<=<a href=").*(?=\">High Quality WMV</a>)' $tmp`
-    title=`grep -oP '(?<=<meta name="title" content=").*(?="/>)' $tmp`
+    title=`grep -oP '(?<=<title>).*(?=</title>)' $tmp | recode HTML_4.0 | sed 's/ |.*//g'`
     rm $tmp
+    echo "Downloading \""$title".wmv\" ..."
     curl -C - -# $url -o "$title.wmv"
 }
 
 for url in "$@"; do
     download_url $url
 done
+
+echo "Done."
