eee6153b68ec6c2b85766eab00e2056fc67a0b25
İsmail Dönmez <ismail@i10z.com>
2014-12-04 22:57:18 +0200

    Make ccl.sh usable on Cygwin

--- ccl.sh
+++ ccl.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 CLANG_EXTRAS="$CLANG_EXTRAS -fno-color-diagnostics"
 
 f=`mktemp /tmp/prog.XXXXXX`
+
+if [ -d /cygdrive ]; then
+    chmod +x $f
+    f=`cygpath -m $f`
+fi
+
 clang++ -std=c++14 $CLANG_EXTRAS -x c++ -o $f -
 
 $f
