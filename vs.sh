4ec3a19389ef820eb080f7545241a066d67c0700
İsmail Dönmez <ismail@i10z.com>
2014-12-06 18:23:35 +0200

    Allow setting ARCH

--- vs.sh
+++ vs.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-cmd /c "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat" "x86" "&&" "C:\\cygwin64\\bin\\zsh" "-i"
+VERSION=12.0
+ARCH=$1
+
+if [ -z $ARCH ]; then
+    ARCH=x86
+fi
+
+cmd /c "C:\\Program Files (x86)\\Microsoft Visual Studio $VERSION\\VC\\vcvarsall.bat" "$ARCH" "&&" "C:\\cygwin64\\bin\\zsh" "--login" "-i"
 
