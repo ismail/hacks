35659576fddbc217c56b4177534c02b59d59e566
İsmail Dönmez <ismail@i10z.com>
2014-12-07 17:32:59 +0200

    Rename build-llvm.bat to build-llvm-windows.sh

---
+++ build-llvm-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -euo pipefail
+
+svn up . tools/clang tools/clang/tools/extra
+
+rm -rf dist
+mkdir dist
+cd dist
+
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_TESTS=ON ..
+
+ninja
+ninja check-all || true
+ninja package
