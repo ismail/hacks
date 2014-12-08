cf1501bc89b129cfc53579e828754b22cee85c33
İsmail Dönmez <ismail@i10z.com>
2014-12-08 15:00:32 +0200

    All tests pass now

--- build-llvm-windows.sh
+++ build-llvm-windows.sh
@@ -11,5 +11,5 @@ export PATH="/cygdrive/c/SysInternals/bin:/cygdrive/c/Program\ Files/SlikSvn/bin
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_TESTS=ON ..
 
 ninja
-ninja check-all || true
+ninja check-all
 ninja package
