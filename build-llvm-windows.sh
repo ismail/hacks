06c2424fccba26bd93fb1f4bed6a56491fbff84c
İsmail Dönmez <ismail@i10z.com>
2014-12-08 15:41:45 +0200

    Revert PATH changes

--- build-llvm-windows.sh
+++ build-llvm-windows.sh
@@ -7,7 +7,7 @@ rm -rf dist
 mkdir dist
 cd dist
 
-export PATH="/cygdrive/c/bin:/cygdrive/c/SysInternals/bin:/cygdrive/c/Program\ Files/SlikSvn/bin":$PATH
+export PATH="/cygdrive/c/Program\ Files/SlikSvn/bin":$PATH
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_TESTS=ON -DLLVM_LIT_ARGS="-s" ..
 
 ninja
