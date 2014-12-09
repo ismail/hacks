e5228db6945f2faf96d9889dc9d7677fd5559471
İsmail Dönmez <ismail@i10z.com>
2014-12-09 14:50:00 +0200

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+cd ~
+mkdir bin
+pushd bin
+
+ln -sf ../hacks/ccl.sh ccl
+ln -sf ../hacks/pull.py pull
+ln -sf ../hacks/qc.py qc
+ln -sf ../hacks/vcpp.py vcpp
+ln -sf ../hacks/weather.py weather
+
+popd
