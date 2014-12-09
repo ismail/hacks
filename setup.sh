3e8e0b20481a9c5f7d401ee8df8735f9afe8ed26
İsmail Dönmez <ismail@i10z.com>
2014-12-09 15:12:02 +0200

    Reorganize source code

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ cd ~
 mkdir -p bin
 pushd bin
 
-ln -sf ../hacks/ccl.sh ccl
-ln -sf ../hacks/mutt-open
-ln -sf ../hacks/pull.py pull
-ln -sf ../hacks/qc.py qc
-ln -sf ../hacks/vcpp.py vcpp
-ln -sf ../hacks/weather.py weather
+ln -sf ../hacks/compilers/ccl.sh ccl
+ln -sf ../hacks/utilities/mutt-open
+ln -sf ../hacks/utilities/pull.py pull
+ln -sf ../hacks/utilities/qc.py qc
+ln -sf ../hacks/compilers/vcpp.py vcpp
+ln -sf ../hacks/utilities/weather.py weather
 
 popd
