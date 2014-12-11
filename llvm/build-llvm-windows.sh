1176d269d87751e4618712f4f92fedfc47674e5d
İsmail Dönmez <ismail@i10z.com>
2014-12-11 16:53:49 +0200

    Switch to git

--- llvm/build-llvm-windows.sh
+++ llvm/build-llvm-windows.sh
@@ -2,20 +2,21 @@
 set -euo pipefail
 version=3.6.0
 
-svn up . tools/clang tools/clang/tools/extra
+pull . tools/clang
 
 rm -rf dist
 mkdir dist
 cd dist
 
 export PATH="/cygdrive/c/Program Files/SlikSvn/bin":$PATH
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON ..
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON .. | tee build.log
 
-ninja | tee build.log
-ninja check-all | tee build.log
-ninja package | tee build.log
+ninja | tee -a build.log
+ninja check-all | tee -a build.log
+ninja package | tee -a build.log
 
-rev=`svn ~/code/llvm/CREDITS.TXT`
-mv LLVM-*.exe ~/dist/LLVM-$version-$rev-win32.exe
+cd ..
+rev=`git log -1 --format="%h"`
+mv dist/LLVM-*.exe ~/dist/LLVM-$version-$rev-win32.exe
 cd ~/dist
 ln -sf LLVM-$version-$rev-win32.exe latest
