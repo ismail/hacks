74ca1fbd0c5f941a11e0f508a8633545a9fd3798
İsmail Dönmez <ismail@i10z.com>
2014-12-11 15:45:52 +0200

    Give me a build log

--- llvm/build-llvm-windows.sh
+++ llvm/build-llvm-windows.sh
@@ -10,8 +10,8 @@ cd dist
 export PATH="/cygdrive/c/Program Files/SlikSvn/bin":$PATH
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON ..
 
-ninja
-ninja check-all
-ninja package
+ninja | tee build.log
+ninja check-all | tee build.log
+ninja package | tee build.log
 
 cp *.exe ~
