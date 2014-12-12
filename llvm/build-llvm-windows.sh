0a3108ad1d12fe40584b98f96a4827d6c6f9f268
İsmail Dönmez <ismail@i10z.com>
2014-12-12 17:31:55 +0200

    Allow test suite failures, capture the log file

--- llvm/build-llvm-windows.sh
+++ llvm/build-llvm-windows.sh
@@ -11,11 +11,13 @@ cd dist
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON .. | tee build.log
 
 ninja | tee -a build.log
-ninja check-all | tee -a build.log
+ninja check-all | tee -a build.log || true
 ninja package | tee -a build.log
 
 cd ..
 rev=`git log -1 --format="%h"`
 mv dist/LLVM-*.exe ~/dist/LLVM-$version-$rev-win32.exe
+mv dist/build.log ~/dist/build-$rev.log
+
 cd ~/dist
 ln -sf LLVM-$version-$rev-win32.exe latest.exe
