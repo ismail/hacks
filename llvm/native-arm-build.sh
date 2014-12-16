e9dc4ad8d1b7e466b11b7c9237bfa3e4aac551cc
İsmail Dönmez <ismail@i10z.com>
2014-12-16 13:28:49 +0200

    keep a build.log

--- llvm/native-arm-build.sh
+++ llvm/native-arm-build.sh
@@ -7,15 +7,15 @@ svn up . tools/clang tools/clang/tools/extra projects/compiler-rt projects/libcx
 rm -rf build; mkdir build; cd build
 
 CFLAGS="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3-d16 -fuse-ld=gold"
-CC=clang CXX=clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=/havana/dist/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCMAKE_C_FLAGS=$CFLAGS -DCMAKE_CXX_FLAGS=$CFLAGS -G "Ninja" ..
+CC=clang CXX=clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=/havana/dist/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCMAKE_C_FLAGS=$CFLAGS -DCMAKE_CXX_FLAGS=$CFLAGS -G "Ninja" .. | tee build.log
 
-ninja
+ninja | tee -a build.log
 
 # Test manually
-./bin/llvm-lit -v -j4 test
-./bin/llvm-lit -v -j4 tools/clang/test
-./bin/llvm-lit -v -j4 projects/libcxx/test
-./bin/llvm-lit -v -j4 projects/libcxxabi/test
+./bin/llvm-lit -v -j4 test | tee -a build.log
+./bin/llvm-lit -v -j4 tools/clang/test | tee -a build.log
+./bin/llvm-lit -v -j4 projects/libcxx/test | tee -a build.log
+./bin/llvm-lit -v -j4 projects/libcxxabi/test | tee -a build.log
 
 ninja install/strip
 
@@ -32,6 +32,7 @@ echo "Compressing with xz..."
 tar cJf llvm-armv7-$version-r"$revision".tar.xz llvm
 echo "Uploading..."
 scp llvm-armv7-$version-r"$revision".tar.xz i10z.com:/havana/llvm/checked
+scp /havana/src/llvm/build/build.log i10z.com:/havana/llvm/checked/latest-build.log
 ssh i10z.com ln -sf /havana/llvm/checked/llvm-armv7-$version-r"$revision".tar.xz  /havana/llvm/checked/latest
 rm llvm-armv7-*
 echo "llvm-armv7-$version-r"$revision".tar.xz uploaded."
