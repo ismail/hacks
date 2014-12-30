ce3c5de8875394707eb922b13729b81d121deb40
İsmail Dönmez <ismail@i10z.com>
2014-12-30 10:07:12 +0200

    Setup build.log early

--- llvm/native-arm-build.sh
+++ llvm/native-arm-build.sh
@@ -3,7 +3,7 @@ set -uo pipefail
 IFS=$'\n\t'
 
 cd /havana/src/llvm
-svn up . tools/clang tools/clang/tools/extra projects/compiler-rt projects/libcxx projects/libcxxabi
+svn up . tools/clang tools/clang/tools/extra projects/compiler-rt projects/libcxx projects/libcxxabi | tee build.log
 
 svnversion CREDITS.TXT > .newbuild
 cmp .newbuild .oldbuild &> /dev/null
@@ -20,15 +20,15 @@ set -e
 rm -rf build; mkdir build; cd build
 
 CFLAGS="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3-d16 -fuse-ld=gold"
-CC=clang CXX=clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_ENABLE_PIC=ON -DLLVM_TARGETS_TO_BUILD=ARM -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=/havana/dist/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCMAKE_C_FLAGS=$CFLAGS -DCMAKE_CXX_FLAGS=$CFLAGS -G "Ninja" .. | tee build.log
+CC=clang CXX=clang++ cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_ENABLE_PIC=ON -DLLVM_TARGETS_TO_BUILD=ARM -DLLVM_ENABLE_TIMESTAMPS=OFF -DCMAKE_INSTALL_PREFIX=/havana/dist/llvm -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DCMAKE_C_FLAGS=$CFLAGS -DCMAKE_CXX_FLAGS=$CFLAGS -G "Ninja" .. | tee -a ../build.log
 
-ninja | tee -a build.log
+ninja | tee -a ../build.log
 
 # Test manually
-./bin/llvm-lit -v -j4 test | tee -a build.log
-./bin/llvm-lit -v -j4 tools/clang/test | tee -a build.log
-./bin/llvm-lit -v -j4 projects/libcxx/test | tee -a build.log
-./bin/llvm-lit -v -j4 projects/libcxxabi/test | tee -a build.log
+./bin/llvm-lit -v -j4 test | tee -a ../build.log
+./bin/llvm-lit -v -j4 tools/clang/test | tee -a ../build.log
+./bin/llvm-lit -v -j4 projects/libcxx/test | tee -a ../build.log
+./bin/llvm-lit -v -j4 projects/libcxxabi/test | tee -a ../build.log
 
 rm -rf /havana/dist/llvm
 ninja install/strip
@@ -49,7 +49,7 @@ echo "Compressing with xz..."
 tar cJf llvm-armv7-$version-r"$revision".tar.xz llvm
 echo "Uploading..."
 scp llvm-armv7-$version-r"$revision".tar.xz i10z.com:/havana/llvm/checked
-scp /havana/src/llvm/build/build.log i10z.com:/havana/llvm/checked/latest-build.log
+scp /havana/src/llvm/build.log i10z.com:/havana/llvm/checked/latest-build.log
 ssh i10z.com ln -sf /havana/llvm/checked/llvm-armv7-$version-r"$revision".tar.xz  /havana/llvm/checked/latest
 rm llvm-armv7-*
 echo "llvm-armv7-$version-r"$revision".tar.xz uploaded."
--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -2,7 +2,7 @@
 set -uo pipefail
 version=3.6.0
 
-pull . tools/clang
+pull . tools/clang | tee build.log
 
 git log -1 --format="%h" > .newbuild
 git --git-dir=./tools/clang/.git log -1 --format="%h" >> .newbuild
@@ -19,16 +19,16 @@ set -e
 
 rm -rf dist; mkdir dist; cd dist
 
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON .. | tee build.log
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=C:/Python27/python.exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
 
-ninja | tee -a build.log
-ninja check-all | tee -a build.log
-ninja package | tee -a build.log
+ninja | tee -a ../build.log
+ninja check-all | tee -a ../build.log
+ninja package | tee -a ../build.log
 
 cd ..
 rev=`git log -1 --format="%h"`
 scp dist/LLVM-*.exe i10z.com:/havana/llvm/win32/LLVM-$version-$rev-win32.exe
-scp dist/build.log i10z.com:/havana/llvm/win32/build-$version-$rev.log
+scp build.log i10z.com:/havana/llvm/win32/build-$version-$rev.log
 ssh i10z.com ln -sf /havana/llvm/win32/LLVM-$version-$rev-win32.exe /havana/llvm/win32/latest.exe
 ssh i10z.com ln -sf /havana/llvm/win32/build-$version-$rev.log /havana/llvm/win32/latest.log
 
