417e3c5edaa02e577bed9df80139e1ebf885f62a
İsmail Dönmez <ismail@i10z.com>
2015-01-07 22:31:22 +0200

    More fixes

--- utilities/noip.sh
+++ utilities/noip.sh
@@ -3,6 +3,6 @@
 username=ismail@donmez.ws
 password=
 host=.ddns.net
-ip=`curl -s whatismyip.akamai.com`
+ip=$(curl -s whatismyip.akamai.com)
 
 curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$host&myip=$ip" > /dev/null
