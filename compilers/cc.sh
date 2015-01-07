51a974fda01f5542f377f0e71ae8c8be437c5363
İsmail Dönmez <ismail@i10z.com>
2015-01-07 20:19:35 +0200

    Modernize ccl.sh

--- compilers/cc.sh
+++ compilers/cc.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 node ~/hacks/compilers/cc.js
-
-
--- compilers/ccl.sh
+++ compilers/ccl.sh
@@ -1,25 +1,21 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -euo pipefail
 trap cleanup EXIT
-
-CLANG_EXTRAS="$CLANG_EXTRAS -fno-color-diagnostics"
 
 cleanup () {
     rm -f $i $f
 }
 
-i=`mktemp /tmp/code.XXXXXX.cpp`
-f=`mktemp /tmp/prog.XXXXXX`
+i=$(mktemp /tmp/code.XXXXXX.cpp)
+f=$(mktemp /tmp/prog.XXXXXX.exe)
 
 if [ -d /cygdrive ]; then
     chmod +x $f
-    i=`cygpath -m $i`
-    f=`cygpath -m $f`
 fi
 
 cat /dev/stdin > $i
 
-clang++ -std=c++14 $CLANG_EXTRAS -o $f $i
+clang++ $CLANG_EXTRAS -o $f(+cyg) $i(+cyg)
 
 $f
