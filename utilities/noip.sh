35773dd93b9b88079d64cdd008dbb9faea806b1b
İsmail Dönmez <ismail@i10z.com>
2015-01-13 20:30:10 +0200

    Support he.net

--- utilities/noip.sh
+++ utilities/noip.sh
@@ -2,7 +2,9 @@
 
 username=ismail@donmez.ws
 password=
-host=.ddns.net
+noip_host=donmez.ddns.net
+he_host=sutoglan.i10z.com
 ip=$(curl -s whatismyip.akamai.com)
 
-curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$host&myip=$ip" > /dev/null
+curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$noip_host&myip=$ip" > /dev/null
+curl "http://dyn.dns.he.net/nic/update?hostname=$he_host&password=$password&myip=$ip" > /dev/null
