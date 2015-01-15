b1acece3c48ca72c7d7365adfdc2898f252bfb70
İsmail Dönmez <ismail@i10z.com>
2015-01-15 13:53:54 +0200

    No TV is better

--- tv/showtv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rtmpdump -r "rtmp://mn-l.mncdn.com/showtv/" -a "showtv/" -f "LNX 12,0,0,44" -W "http://static.oroll.com/p.swf" -p "http://www.showtv.com.tr/canli-yayin/showtv" -y "showtv2" - | mpv -
