99aca5f6765133aa127eeb77358bcb213bc7cd9c
İsmail Dönmez <ismail@i10z.com>
2015-01-16 16:25:22 +0200

    Update ddns.sh

--- utilities/ddns.sh
+++ utilities/ddns.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -u
 
 username=ismail@donmez.ws
 password=
@@ -6,5 +7,10 @@ noip_host=donmez.ddns.net
 he_host=sutoglan.i10z.com
 ip=$(curl -s whatismyip.akamai.com)
 
+if [ -z $password ]; then
+    echo "Password can't be empty."
+    exit 0
+fi
+
 curl -s -u $username:"$password" "http://dynupdate.no-ip.com/nic/update?hostname=$noip_host&myip=$ip" > /dev/null
-curl "http://dyn.dns.he.net/nic/update?hostname=$he_host&password="$password"&myip=$ip" > /dev/null
+curl -s "http://dyn.dns.he.net/nic/update?hostname=$he_host&password="$password"&myip=$ip" > /dev/null
