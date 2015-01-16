1b602ee29f446a659e73a593ab9e146e2b2d5679
İsmail Dönmez <ismail@i10z.com>
2015-01-16 15:31:58 +0200

    Missed this part

--- utilities/ddns.sh
+++ utilities/ddns.sh
@@ -7,4 +7,4 @@ he_host=sutoglan.i10z.com
 ip=$(curl -s whatismyip.akamai.com)
 
 curl -s -u $username:"$password" "http://dynupdate.no-ip.com/nic/update?hostname=$noip_host&myip=$ip" > /dev/null
-curl "http://dyn.dns.he.net/nic/update?hostname=$he_host&password=$password&myip=$ip" > /dev/null
+curl "http://dyn.dns.he.net/nic/update?hostname=$he_host&password="$password"&myip=$ip" > /dev/null
