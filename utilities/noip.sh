67313fca352008394a8b84aa6b78b6b06f7bf668
İsmail Dönmez <ismail@i10z.com>
2015-01-16 15:30:43 +0200

    Quote the password

--- utilities/noip.sh
+++ utilities/noip.sh
@@ -6,5 +6,5 @@ noip_host=donmez.ddns.net
 he_host=sutoglan.i10z.com
 ip=$(curl -s whatismyip.akamai.com)
 
-curl -s -u $username:$password "http://dynupdate.no-ip.com/nic/update?hostname=$noip_host&myip=$ip" > /dev/null
+curl -s -u $username:"$password" "http://dynupdate.no-ip.com/nic/update?hostname=$noip_host&myip=$ip" > /dev/null
 curl "http://dyn.dns.he.net/nic/update?hostname=$he_host&password=$password&myip=$ip" > /dev/null
