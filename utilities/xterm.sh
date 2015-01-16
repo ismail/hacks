7fb9f2a34969f940546c7b5cf63db62ef9b55980
İsmail Dönmez <ismail@i10z.com>
2015-01-16 15:26:34 +0200

    Update geometry

--- utilities/xterm.sh
+++ utilities/xterm.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-/usr/bin/xterm +bc +cm +fbx +sb -uc -fa "Consolas" -fs 13 -geometry 127x59+70+41 -e tmux-shell
-
+/usr/bin/xterm +bc +cm +fbx +sb -uc -fa "Consolas" -fs 13 -geometry 127x59--6--9 -e tmux-shell
