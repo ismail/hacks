7b22ecccbe803be07938efca144f76684ca2a10e
İsmail Dönmez <ismail@i10z.com>
2015-01-31 14:03:26 +0200

    Switch to nmake

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -35,12 +35,12 @@ rm -rf dist; mkdir dist; cd dist
 
 export CC="$(cygpath -m "$(whence cl.exe)")"
 export CXX=$CC
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
 
-ninja | tee -a ../build.log
+nmake | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v test | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v tools/clang/test | tee -a ../build.log
-ninja package | tee -a ../build.log
+nmake package | tee -a ../build.log
 
 cd ..
 rev=$(git log -1 --format="%h")
