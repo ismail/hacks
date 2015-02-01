bc0ccd0746840a22ea6e5bf75b8d58964ec00d96
İsmail Dönmez <ismail@i10z.com>
2015-02-01 11:19:53 +0200

    Add vk.sh

---
+++ utilities/vk.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+username=
+password=
+base="http://vk.com/video_ext.php?"
+values=($(curl -s $1 | grep -o -E "oid=.*hd=" | awk -F '&#038;' '{print $1" "$2" "$3}'))
+
+youtube-dl "$base$values[1]&$values[2]&$values[3]&api_hash=&hd=1" --username=$username --password=$password
