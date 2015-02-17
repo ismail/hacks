0629ff65cc94f32905aac0e8735ee9d32d64ad6e
İsmail Dönmez <ismail@i10z.com>
2015-02-17 22:16:08 +0200

    Use static linking

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -35,7 +35,7 @@ rm -rf dist; mkdir dist; cd dist
 
 export CC="$(cygpath -m "$(whence cl.exe)")"
 export CXX=$CC
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
 
 ninja -j1 | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v test | tee -a ../build.log
