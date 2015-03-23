7af5f5a597ba4dc99e59c658523fea1265612b94
İsmail Dönmez <ismail@i10z.com>
2015-03-23 11:50:52 +0200

    Rename tmux

--- utilities/xterm.sh
+++ utilities/xterm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/xterm +bc +cm +fbx +sb -uc -fa "Consolas" -fs 13 -geometry 127x59--6--9 -e tmux-shell
+/usr/bin/xterm +bc +cm +fbx +sb -uc -fa "Consolas" -fs 13 -geometry 127x59--6--9 -e ~/bin/tmux
