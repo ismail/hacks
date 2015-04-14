29c0451481a72981e2156295b9ea8268658def6c
İsmail Dönmez <ismail@i10z.com>
2015-04-14 15:40:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,11 @@ mkdir -p bin
 pushd bin
 
 ln -sf ../hacks/compilers/ccl.sh ccl
+ln -sf ../hacks/compilers/vcpp.py vcpp
 ln -sf ../hacks/utilities/mutt-open
 ln -sf ../hacks/utilities/pull.py pull
 ln -sf ../hacks/utilities/qc.py qc
-ln -sf ../hacks/compilers/vcpp.py vcpp
 ln -sf ../hacks/utilities/weather.py weather
+ln -sf ../hacks/utilities/tmux-shell tmuxsh
 
 popd
