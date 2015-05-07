b48c7e121d2cbfffefb952627d9ff8ee45419395
İsmail Dönmez <ismail@i10z.com>
2015-05-07 16:26:53 +0300

    Cleanup xterm.sh

--- utilities/xterm.sh
+++ utilities/xterm.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-/usr/bin/xterm +bc +cm +fbx +sb -uc -wc -u8 -fa "Consolas" -fd "Consolas" -fs 13 -geometry 127x59--6--9 -e ~/bin/tmuxsh
+font="Consolas"
+font_size=13
+geometry="127x59--6--9"
+
+exec /usr/bin/xterm \
+     +bc \
+     +cm \
+     +fbx \
+     +sb \
+     -uc \
+     -wc \
+     -u8 \
+     -fa $font \
+     -fd $font \
+     -fs $font_size \
+     -geometry $geometry \
+     -e ~/bin/tmuxsh
