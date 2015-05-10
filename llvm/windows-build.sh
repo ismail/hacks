ac58208333ac9623fd5ef987abb8e435336c8eb9
İsmail Dönmez <ismail@i10z.com>
2015-05-10 21:02:08 +0300

    Update llvm windows build script

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 version=3.7
 src=~/code/llvm
 target=${1:-win32}
-python_exe=C:/Python27/python.exe
+python_exe=C:/Python34/python.exe
 
 function cleanup {
     cd $src
@@ -41,18 +41,9 @@ rm -rf dist; mkdir dist; cd dist
 
 export CC="$(cygpath -m =cl.exe)"
 export CXX=$CC
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
 
 ninja -j1 | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v test | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v tools/clang/test | tee -a ../build.log
 ninja package | tee -a ../build.log
-
-cd ..
-rev=$(git log -1 --format="%h")
-scp dist/LLVM-*.exe i10z.com:/havana/llvm/$target/LLVM-$version-$rev-$target.exe
-scp build.log i10z.com:/havana/llvm/$target/latest.log
-ssh i10z.com ln -sf /havana/llvm/$target/LLVM-$version-$rev-$target.exe /havana/llvm/$target/latest.exe
-
-# Bintray upload
-curl -T dist/LLVM-*-$target.exe -uismail:$(cat ~/.bintray) -H X-Bintray-Package:llvm-$target -H X-Bintray-Version:$version-$rev -H X-Bintray-Publish:1 https://api.bintray.com/content/ismail/llvm/LLVM-$version-$rev-$target.exe
