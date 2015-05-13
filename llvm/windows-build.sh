50e9f1daba198a7444264e1e3a68b549140f564b
İsmail Dönmez <ismail@i10z.com>
2015-05-13 16:17:58 +0300

    [llvm/windows-build.sh] Let ninja use all cores

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -43,7 +43,7 @@ export CC="$(cygpath -m =cl.exe)"
 export CXX=$CC
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DLLVM_ENABLE_TIMESTAMPS=OFF -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DPYTHON_EXECUTABLE=$python_exe -DLLVM_BUILD_TESTS=ON .. | tee -a ../build.log
 
-ninja -j1 | tee -a ../build.log
+ninja | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v test | tee -a ../build.log
 $python_exe -u ./bin/llvm-lit.py -v tools/clang/test | tee -a ../build.log
 ninja package | tee -a ../build.log
