435f43c0b115a63163d677d5dac7b8f85609604e
İsmail Dönmez <ismail@i10z.com>
2015-05-23 12:29:56 +0300

    Update mingw.bat

--- mingw/mingw.bat
+++ mingw/mingw.bat
@@ -3,5 +3,3 @@ set GCC_VERSION=5.1.1
 set GCC_ROOT=C:\mingw-w64-5.1.1
 
 set PATH=%PATH%;%GCC_ROOT%\bin;%GCC_ROOT%libexec\gcc\x86_64-w64-mingw32\%GCC_VERSION%
-cmd.exe /K "cd /d C:\Users\ismail\Desktop"
-
