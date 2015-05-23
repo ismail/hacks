bd517703545b7b676280e2b1797fd063a36db74e
İsmail Dönmez <ismail@i10z.com>
2015-05-23 20:28:07 +0300

    [mingw/build-mingw64.sh] Cleanup

--- mingw/build-mingw64.sh
+++ mingw/build-mingw64.sh
@@ -5,6 +5,7 @@ GCC_VERSION=5.1.1
 GDB_VERSION=7.9.1
 SRC_ROOT=/havana/mingw-w64-build
 INSTALL_ROOT=/havana/mingw-w64-$GCC_VERSION
+TARGET=x86_64-w64-mingw32
 
 rm -rf $INSTALL_ROOT
 cd $SRC_ROOT
@@ -12,13 +13,13 @@ pull gcc-5-branch mingw-w64
 
 cd mingw-w64
 rm -rf build; mkdir build; cd build
-../configure --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --prefix=/havana/mingw-w64/x86_64-w64-mingw32 --disable-lib32
+../configure --host=$TARGET --target=$TARGET --prefix=$INSTALL_ROOT/x86_64-w64-mingw32 --disable-lib32
 make -j$(nproc)
 make install-strip
 
 cd ../mingw-w64-libraries/winpthreads
 rm -rf build; mkdir build; cd build
-../configure --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --prefix=/havana/mingw-w64/x86_64-w64-mingw32
+../configure --host=$TARGET --target=$TARGET --prefix=$INSTALL_ROOT/x86_64-w64-mingw32
 make -j$(nproc)
 make install-strip
 
@@ -38,10 +39,10 @@ cd ..
 
 rm -rf build; mkdir build; cd build
 
-../combined/configure --build=x86_64-suse-linux-gnu --host=x86_64-w64-mingw32 \
-                      --prefix=/havana/mingw-w64 --with-sysroot=/havana/mingw-w64 \
-                      --target=x86_64-w64-mingw32 --disable-gcov-tool --disable-multilib \
-                      --disable-nls --disable-win32-registry --enable-checking=release \
+../combined/configure --build=x86_64-suse-linux-gnu --host=$TARGET --target=$TARGET \
+                      --prefix=$INSTALL_ROOT --with-sysroot=$INSTALL_ROOT \
+                      --disable-gcov-tool --disable-multilib --disable-nls \
+                      --disable-win32-registry --enable-checking=release \
                       --enable-languages=c,c++,fortran --enable-fully-dynamic-string \
                       --enable-libgomp --enable-threads=win32 --disable-werror \
                       --disable-libvtv --with-arch=corei7 --with-tune=haswell
@@ -51,17 +52,19 @@ make install
 
 cd ..
 cd gdb-$GDB_VERSION
-./configure --build=x86_64-suse-linux-gnu --host=x86_64-w64-mingw32 --prefix=/havana/mingw-w64
+rm -rf build; mkdir build; cd build
+../configure --host=$TARGET --target=$TARGET --prefix=$INSTALL_ROOT
 make -j$(nproc)
 make install
 
 cd $INSTALL_ROOT
-x86_64-w64-mingw32-strip bin/* libexec/gcc/x86_64-w64-mingw32/$GCC_VERSION/* || true
-rm -rf libexec/gcc/x86_64-w64-mingw32/$GCC_VERSION/install-tools
+$TARGET-strip bin/* libexec/gcc/$TARGET/$GCC_VERSION/* || true
+rm -rf libexec/gcc/$TARGET/$GCC_VERSION/install-tools
 rm -rf mingw/include
 rm bin/ld.bfd.exe
 cd ..
 
-tar --exclude-vcs -cf mingw-w64-$GCC_VERSION.tar mingw-w64-$GCC_VERSION
-xz -6 -T0 mingw-w64-$GCC_VERSION.tar
-mv mingw-w64-$GCC_VERSION.tar.xz $HOME
+f=mingw-w64-$GCC_VERSION-$(date +%Y%m%d)
+tar --exclude-vcs -cf $f.tar mingw-w64-$GCC_VERSION
+xz -6 -T0 $f.tar
+mv $f.tar.xz $HOME
