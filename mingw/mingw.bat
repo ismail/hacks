a083fd9ddf82b2a673abaa90dabd70976374e149
İsmail Dönmez <ismail@i10z.com>
2015-06-10 20:09:19 +0300

    [mingw.bat] Support passing an argument

--- mingw/mingw.bat
+++ mingw/mingw.bat
@@ -1,5 +1,10 @@
 @echo off
-set GCC_VERSION=5.1.1
-set GCC_ROOT=C:\mingw-w64-5.1.1
+IF [%1]==[] (
+    set GCC_VERSION=5.1.1
+    ) ELSE (
+    set GCC_VERSION=%1
+    )
 
-set PATH=%PATH%;%GCC_ROOT%\bin;%GCC_ROOT%libexec\gcc\x86_64-w64-mingw32\%GCC_VERSION%
+set GCC_ROOT=C:\mingw-w64-%GCC_VERSION%
+
+set PATH=%PATH%;%GCC_ROOT%\bin;%GCC_ROOT%\libexec\gcc\x86_64-w64-mingw32\%GCC_VERSION%
