c79e84c823906c53c2c823e53812fe9b4282528e
İsmail Dönmez <ismail@i10z.com>
2015-07-21 16:48:24 +0300

    [llvm/windows-build.sh] Minor change to sleep routine

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -20,8 +20,8 @@ current_time=$(date +%s)
 time_diff=$(( (current_time - last_build_time) / (60*60) ))
 
 if [ $time_diff -lt $wait_hours ]; then
-    echo "Last successful build was less than $wait_hours hours ago, sleeping for 1 hour."
-    sleep 1h
+    echo "Last successful build was less than $wait_hours hours ago, sleeping for $wait_hours hours."
+    sleep ${wait_hours}h
     exit 0
 fi
 
