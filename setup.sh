cb1f6a5c21a062e56ec525bbea1bf43a1e3b791c
İsmail Dönmez <ismail@i10z.com>
2015-07-28 15:39:52 +0300

    [setup.sh] Sort and add ldd-windows

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@ pushd bin
 
 ln -sf ../hacks/compilers/ccl.sh ccl
 ln -sf ../hacks/compilers/vcpp.py vcpp
+ln -sf ../hacks/utilities/ldd-windows
 ln -sf ../hacks/utilities/mutt-open
-ln -sf ../hacks/utilities/pull.py pull
-ln -sf ../hacks/utilities/qc.py qc
-ln -sf ../hacks/utilities/weather.py weather
-ln -sf ../hacks/utilities/tmux-shell tmuxsh
 ln -sf ../hacks/utilities/pb pbcopy
 ln -sf ../hacks/utilities/pb pbpaste
+ln -sf ../hacks/utilities/pull.py pull
+ln -sf ../hacks/utilities/qc.py qc
+ln -sf ../hacks/utilities/tmux-shell tmuxsh
+ln -sf ../hacks/utilities/weather.py weather
 
 popd
