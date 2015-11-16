3fa0bc8627e41f4944250a840beceb47b415176b
İsmail Dönmez <ismail@i10z.com>
2015-11-16 13:02:58 +0200

    Do not to hardcode paths

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env zsh
 
-cd ~
+curdir=$(pwd)
+cd $HOME
+
 mkdir -p bin
 pushd bin
 
-ln -sf ../hacks/compilers/ccl.sh ccl
-ln -sf ../hacks/compilers/vcpp.py vcpp
-ln -sf ../hacks/utilities/ldd-windows
-ln -sf ../hacks/utilities/mutt-open
-ln -sf ../hacks/utilities/pb pbcopy
-ln -sf ../hacks/utilities/pb pbpaste
-ln -sf ../hacks/utilities/pull.py pull
-ln -sf ../hacks/utilities/qc.py qc
-ln -sf ../hacks/utilities/tmux-shell tmuxsh
-ln -sf ../hacks/utilities/weather.py weather
+ln -sf $curdir/compilers/ccl.sh ccl
+ln -sf $curdir/compilers/vcpp.py vcpp
+ln -sf $curdir/utilities/ldd-windows
+ln -sf $curdir/utilities/mutt-open
+ln -sf $curdir/utilities/pb pbcopy
+ln -sf $curdir/utilities/pb pbpaste
+ln -sf $curdir/utilities/pull.py pull
+ln -sf $curdir/utilities/qc.py qc
+ln -sf $curdir/utilities/tmux-shell tmuxsh
+ln -sf $curdir/utilities/weather.py weather
 
 popd
