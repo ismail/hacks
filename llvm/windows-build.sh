470f78dc093fc3eab757e34fb1532caeb50d6fa3
İsmail Dönmez <ismail@i10z.com>
2015-12-13 18:47:53 +0200

    Minor change

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -61,7 +61,7 @@ git --git-dir=./tools/clang/.git log -1 --format="%h" >> .newbuild
 cmp -s .newbuild .oldbuild
 
 if [ $? = 0 ]; then
-    echo "No new build. Sleeping for 1 hour" | tee -a build.log
+    echo "No new build. Sleeping for 1 hour." | tee -a build.log
     sleep 1h
     exit 0
 fi
