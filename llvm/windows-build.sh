d6a1da79c3d3c85d6f6458f86f523781ffb3084f
İsmail Dönmez <ismail@i10z.com>
2016-02-10 20:25:59 +0200

    llvm build: remove VS2015 hack, should be fixed with llvm r260396

--- llvm/windows-build.sh
+++ llvm/windows-build.sh
@@ -50,9 +50,8 @@ if [ "x$current_date" = "x$last_build_date" ]; then
 fi
 
 cd $src
+git checkout -f
 pull . tools/{clang,clang/tools/extra,lld} projects/{compiler-rt,openmp} | tee build.log
-# XXX: HACK for VS2015
-rm -f test/DebugInfo/PDB/DIA/pdbdump-symbol-format.test
 
 git log -1 --format="%h" > .newbuild
 git --git-dir=./tools/clang/.git log -1 --format="%h" >> .newbuild
