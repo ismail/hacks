fcd2e9cdc8b8fe00377ee5aa758771a472f514f2
İsmail Dönmez <ismail@i10z.com>
2016-02-13 19:18:01 +0200

    Merge mailer.py into kindleit.py, for real this time

--- utilities/kindleit.py
+++ utilities/kindleit.py
@@ -1,14 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from mailer import mail
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
 import os
 import sys
+import smtplib
 
-gmail_user = "ismail@donmez.ws"
+gmail_user = "ismail@i10z.com"
 gmail_pwd =  open("%s/.gpass" % os.environ["HOME"] ).readlines()[0].strip("\r\n")
 subject = "convert"
 recipient = "donmez@free.kindle.com"
+
+def mail(username, password, to, subject, text, attach):
+   msg = MIMEMultipart()
+
+   msg['From'] = username
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   part = MIMEBase('application', 'octet-stream')
+   part.set_payload(open(attach, 'rb').read())
+   encoders.encode_base64(part)
+   part.add_header('Content-Disposition',
+           'attachment; filename="%s"' % os.path.basename(attach))
+   msg.attach(part)
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(username, password)
+   mailServer.sendmail(username, to, msg.as_string())
+   mailServer.quit()
 
 if __name__ == "__main__":
 
