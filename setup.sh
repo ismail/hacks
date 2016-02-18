7afd1e99f0813664aee3f0350948b85ad1bc4fe3
İsmail Dönmez <ismail@i10z.com>
2016-02-18 15:00:15 +0200

    Remove qc from setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ ln -sf $curdir/utilities/mutt-open
 ln -sf $curdir/utilities/pb pbcopy
 ln -sf $curdir/utilities/pb pbpaste
 ln -sf $curdir/utilities/pull.py pull
-ln -sf $curdir/utilities/qc.py qc
 ln -sf $curdir/utilities/tmux-shell tmuxsh
 
 popd
