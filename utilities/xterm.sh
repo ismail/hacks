4e92ec28b87285f8add3702a0fb1fbdabbf6b753
İsmail Dönmez <ismail@i10z.com>
2016-03-23 14:57:02 +0200

    Simplify xterm script

--- utilities/xterm.sh
+++ utilities/xterm.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-font="Consolas"
-font_size=13
 geometry="117x49+114-53"
 
-exec /usr/bin/xterm \
-     +bc \
-     +cm \
-     -lc \
-     +sb \
-     -uc \
-     -fa $font \
-     -fd $font \
-     -fs $font_size \
-     -geometry $geometry \
-     -ti vt340 \
-     "$@"
+setsid /usr/bin/xterm \
+       -geometry $geometry \
+       "$@"
