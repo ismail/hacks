215a31e232d6dfb3998054531d1885ac5795a1a8
İsmail Dönmez <ismail@i10z.com>
2016-08-18 10:32:05 +0300

    Fold lines at max 80 characters

--- utilities/vim-changelog.sh
+++ utilities/vim-changelog.sh
@@ -7,5 +7,5 @@ echo "Updated to revision $2, fixes the following problems"
 for i in {$1..$2}; do
     curl $directory/$version/$version.$i -s | grep -v "Binary file (standard input) matches" | \
     tr -d '\n' | grep -oP "Problem:.*Solution:" | sed s,"Problem:    ","  * ", | sed s,"Solution:",, | \
-    tr '\t' '\n' | sed  s,'           ','',
+    tr '\t' '\n' | sed  s,'           ','', | fmt -w 80
 done
