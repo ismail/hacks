2194cb71bb950a8b4d67d2bee8094521bd903eeb
İsmail Dönmez <ismail@i10z.com>
2017-09-06 15:33:14 +0200

    Add conjugation utility using verbformen.de

---
+++ utilities/konj.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from urllib.parse import urlencode, quote
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup as soup
+import sys
+
+filtered_tenses = ["Perfekt", "Präteritum"]
+
+def findKonjugation(string):
+    req = Request("https://www.verbformen.de/konjugation/?w=%s" % string )
+    s=soup(urlopen(req).read(), "lxml")
+
+    for table in s.findAll('section', attrs={'class': 'rBox rBoxWht'}):
+        div_id = table.find('div', attrs={'id': 'indikativ'})
+
+        if div_id is not None:
+            for column in table.findAll('div', attrs={'class': 'vTbl'}):
+                tense = column.find('h3').text
+                if tense in filtered_tenses:
+                    print(f"-> {tense}")
+                    for tr in column.findAll('tr'):
+                        print(tr.text)
+                    print("")
+
+if __name__ == "__main__":
+    try:
+        findKonjugation(sys.argv[1])
+    except IndexError:
+        print("Please pass an argument.")
+
