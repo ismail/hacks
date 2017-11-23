945cecb48a0fb327617313fc8d31667e2201af39
İsmail Dönmez <ismail@i10z.com>
2017-11-23 13:31:28 +0100

    Handle HTTP errors, stop using italics

--- utilities/tdk.py
+++ utilities/tdk.py
@@ -5,6 +5,7 @@
 
 from urllib.parse import urlencode
 from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 import sys
 
 URL="http://www.tdk.gov.tr/index.php?option=com_gts&arama=gts&guid=TDK.GTS.546b3e9c2e0046.90953438"
@@ -18,7 +19,13 @@ except ImportError:
 def searchWord(word):
     postData = {'kelime' : word}
     req = Request(URL, urlencode(postData).encode("iso-8859-9"))
-    result = urlopen(req).read()
+
+    try:
+        result = urlopen(req).read()
+    except HTTPError as e:
+        print(e)
+        sys.exit(-1)
+
     results = soup(result, "lxml").findAll('table',attrs={'id' : 'hor-minimalist-a'})
     if results:
         resultTable = results[0]
@@ -28,9 +35,7 @@ def searchWord(word):
 
     for td in resultTable.findAll('td'):
         text = td.text.strip()
-        # Ansi escape code for italic
-        text=text.replace('"', '\n\x1B[3m"', 1)
-        text+='\x1B[23m'
+        text = text.replace('"', '\n"', 1)
         print("%s\n" % text)
 
 if __name__ == "__main__":
