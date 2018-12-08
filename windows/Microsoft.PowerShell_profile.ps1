fbf80b32acfd21bc4766c537996a1dbe84cf820c
İsmail Dönmez <ismail@i10z.com>
2018-12-09 00:36:09 +0100

    Make powershell use UTF-8 for git

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -36,6 +36,7 @@ function activate ( $venv )
 }
 
 [console]::OutputEncoding = [Text.Encoding]::Utf8
+$env:LC_ALL='C.UTF-8'
 
 set-psreadlineoption -BellStyle None
 set-psreadlinekeyhandler -Key UpArrow -Function HistorySearchBackward
