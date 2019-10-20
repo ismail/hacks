0375f77a4e0424135dd0ae17633076b0a845e1ec
İsmail Dönmez <ismail@i10z.com>
2019-10-20 20:06:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ cd $HOME
 mkdir -p bin
 pushd bin
 
-ln -sf $curdir/utilities/ldd-windows
-ln -sf $curdir/utilities/mutt-open
-ln -sf $curdir/utilities/pb pbcopy
-ln -sf $curdir/utilities/pb pbpaste
-ln -sf $curdir/utilities/pull.py pull
-ln -sf $curdir/utilities/tmux-shell tmuxsh
+ln -sf $curdir/mutt-open
+ln -sf $curdir/pb pbcopy
+ln -sf $curdir/pb pbpaste
+ln -sf $curdir/pull.py pull
+ln -sf $curdir/tmux-shell tmuxsh
 
 popd
