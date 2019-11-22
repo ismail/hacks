0f5b76dd94478516ba9a92d8b89bf0383e5b4823
İsmail Dönmez <ismail@i10z.com>
2019-11-22 16:39:18 +0100

    Add a comment

--- gdm-disable-suspend.sh
+++ gdm-disable-suspend.sh
@@ -1,3 +1,4 @@
+# https://gitlab.gnome.org/GNOME/gnome-control-center/issues/22
 sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'nothing'
 sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
