3f6052d9fbe1818366233921a133dd24a19f8731
İsmail Dönmez <ismail@i10z.com>
2021-03-20 14:03:47 +0100

    Move sysrooter.sh

--- sysrooter.sh
+++ sysrooter.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/zsh
+set -euo errexit
 
 if [[ $(whoami) = "root" ]]; then
     echo "Don't run this script as root."
     exit -1
 fi
 
-ARCH=aarch64
-#ARCH=armv7hl
+ARCH=$1
 REPOURL=http://download.opensuse.org/ports/$ARCH/tumbleweed/repo/oss
 TARGET=$HOME/$ARCH.sysroot
+
+# Create a fake zypp.conf
+conf=$(mktemp)
+cat << EOF > $conf
+[main]
+arch=$ARCH
+EOF
+
+# Shortcut
+function run_zypper {
+    sudo ZYPP_CONF=$conf zypper --non-interactive --no-gpg-checks --root $TARGET "$@"
+}
 
 # Clean up
 sudo rm -rf $TARGET/{bin,boot,etc,home,lib,mnt,opt,root,run,sbin,srv,tmp,usr,var}
 
 # Add default OSS repo
-sudo zypper --root $TARGET ar $REPOURL repo-oss
-sudo zypper --non-interactive --no-gpg-checks --root $TARGET ref
+run_zypper ar $REPOURL repo-oss
+run_zypper ref
 
 # Copy emulation binaries
 sudo mkdir -p $TARGET/usr/bin
@@ -25,8 +37,7 @@ sudo cp /usr/bin/emu /usr/bin/qemu-$ARCH $TARGET/usr/bin
 sudo mkdir -p $TARGET/{dev,proc,sys}
 
 # zypper mounts /proc itself
-sudo mount --bind /dev $TARGET/dev || :
+mount -l | grep "$TARGET/dev" &>/dev/null || sudo mount --bind /dev $TARGET/dev
 
 # Install bash and some other required packages
-sudo zypper --non-interactive --root $PWD/$ARCH.sysroot in bash
-sudo zypper --non-interactive --root $PWD/$ARCH.sysroot in glibc-locale-base terminfo
+run_zypper in bash glibc-locale-base terminfo
