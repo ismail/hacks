2a8ac02831ab306e1ce85b12ec9a42b933b530a3
İsmail Dönmez <ismail@i10z.com>
2022-03-01 22:16:01 +0100

    Remove some cruft

--- gdm-disable-suspend.sh
+++
@@ -1,5 +0,0 @@
-# https://gitlab.gnome.org/GNOME/gnome-control-center/issues/22
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'nothing'
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
-sudo -u gdm dbus-launch gsettings list-recursively org.gnome.settings-daemon.plugins.power
--- max-pstate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $1 = "-r" ]]; then
-    sudo cpupower frequency-set -g powersave
-else
-    sudo cpupower frequency-set -g performance
-fi
