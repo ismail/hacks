480480750e91787cba9cc670b65e0a2be27f44bf
İsmail Dönmez <ismail@i10z.com>
2022-09-30 19:52:54 +0200

    Reformat

--- fortune.py
+++ fortune.py
@@ -126,7 +126,10 @@ quotes = {
     "Arthur Schopenhauer",
     "Unglücklich das Land, das Helden nötig hat.":
     "Bertolt Brecht",
-    "The heart has its reasons, which reason does not know... We know truth, not only by the reason, but also by the heart.":
+    """
+    The heart has its reasons, which reason does not know...
+    We know truth, not only by the reason, but also by the heart.
+    """:
     "Blaise Pascal",
     """
     When you are dead, you do not know you are dead. It's only painful & difficult for others.
@@ -137,7 +140,10 @@ quotes = {
     "A. E. Housman",
     "Das Schönste und Tiefste, was der Mensch erleben kann, ist das Gefühl des Geheimnisvollen.":
     "Albert Einstein",
-    "The tree which moves some to tears of joy is in the eyes of others only a green thing which stands in the way... As a man is, so he sees.":
+    """
+    The tree which moves some to tears of joy is in the eyes of others only a green thing which stands in the way...
+    As a man is, so he sees.
+    """:
     "William Blake",
     "The mind is a wonderful servant but a terrible master.":
     "",
