b6bb31881810b1275de60379f4d89f0772f1ad8a
İsmail Dönmez <ismail@i10z.com>
2023-05-01 19:58:32 +0200

    Add scripts import/export Chrome search engine settings

---
+++ import_search_engines.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+SOURCE=${1:-~/keywords.sql}
+TEMP_SQL_SCRIPT=/tmp/sync_chrome_sql_script
+echo
+echo "Importing Chrome keywords from $SOURCE..."
+cd ~/Library/Application\ Support/Microsoft\ Edge/Default
+echo DROP TABLE IF EXISTS keywords\; > $TEMP_SQL_SCRIPT
+echo .read $SOURCE >> $TEMP_SQL_SCRIPT
+sqlite3 -init $TEMP_SQL_SCRIPT Web\ Data .exit
+rm $TEMP_SQL_SCRIPT
