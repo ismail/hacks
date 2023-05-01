a430890543b08bc9a2caf1ece11879c0b2122630
İsmail Dönmez <ismail@i10z.com>
2023-05-01 19:57:40 +0200

    Add a patch from GPT-4 to support custom port and directory

--- serve.go
+++ serve.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
-	port := ":8080"
-	handler := http.FileServer(http.Dir(os.Args[1]))
-	http.ListenAndServe(port, handler)
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		log.Fatal("Please provide a directory as the first argument.")
+	}
+
+	dir := args[0]
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+
+	port := 8080
+	if len(args) >= 2 {
+		var err error
+		port, err = strconv.Atoi(args[1])
+		if err != nil {
+			log.Fatalf("Invalid port: %s. Please provide a valid integer.", args[1])
+		}
+	}
+
+	fmt.Printf("Serving files from '%s' on port %d\n", dir, port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
