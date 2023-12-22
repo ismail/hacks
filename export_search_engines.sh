18904a1bda33426d205e821e708e1c08e8931409
İsmail Dönmez <ismail@i10z.com>
2023-12-22 12:09:21 +0100

    Add Chrome search engine import/export scripts

--- export_search_engines.sh
+++ export_search_engines.sh
@@ -3,7 +3,7 @@
 DESTINATION=${1:-~/keywords.sql}
 TEMP_SQL_SCRIPT=/tmp/sync_chrome_sql_script
 echo "Exporting Chrome keywords to $DESTINATION..."
-cd "Library/Application Support/Google/Chrome/Default"
+cd ~/Library/Application\ Support/Google/Chrome/Default
 echo .output $DESTINATION > $TEMP_SQL_SCRIPT
 echo .dump keywords >> $TEMP_SQL_SCRIPT
 sqlite3 -init $TEMP_SQL_SCRIPT Web\ Data .exit
--- import_search_engines.sh
+++ import_search_engines.sh
@@ -4,7 +4,7 @@ SOURCE=${1:-~/keywords.sql}
 TEMP_SQL_SCRIPT=/tmp/sync_chrome_sql_script
 echo
 echo "Importing Chrome keywords from $SOURCE..."
-cd ~/Library/Application\ Support/Microsoft\ Edge/Default
+cd ~/Library/Application\ Support/Google/Chrome/Default
 echo DROP TABLE IF EXISTS keywords\; > $TEMP_SQL_SCRIPT
 echo .read $SOURCE >> $TEMP_SQL_SCRIPT
 sqlite3 -init $TEMP_SQL_SCRIPT Web\ Data .exit
